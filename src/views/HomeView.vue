91039d67d185f2ba1c305e16f37256821d74db09
Benison Abraham <benisonkt@gmail.com>
2026-01-10 15:27:04 +0000

    Add Tailwind CSS integration and update HomeView styling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>Home</h1>
+  <h1 class="text-3xl font-bold underline">Home</h1>
   <RouterLink to="/login">Login</RouterLink>
   <RouterLink to="/register">Register</RouterLink>
 </template>
