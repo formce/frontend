2b927f90413a527d92a92d3928f0022734cef3ed
Benison Abraham <benisonkt@gmail.com>
2026-01-10 15:23:24 +0000

    Add routing and views for home, login, register, and form management

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,43 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('@/views/HomeView.vue'),
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/LoginView.vue'),
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('@/views/RegisterView.vue'),
+    },
+    {
+      path: '/form/dashboard',
+      name: 'dashboard',
+      component: () => import('@/views/Form/DashboardView.vue'),
+    },
+    {
+      path: '/form/:formId/edit',
+      name: 'editForm',
+      component: () => import('@/views/Form/EditView.vue'),
+    },
+    {
+      path: '/form/:formId',
+      name: 'publicForm',
+      component: () => import('@/views/Form/PublicView.vue'),
+    },
+    {
+      path: '/form/:formId/response',
+      name: 'formResponses',
+      component: () => import('@/views/Form/ResponsesView.vue'),
+    },
+  ],
 })
 
 export default router
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Home</h1>
+  <RouterLink to="/login">Login</RouterLink>
+  <RouterLink to="/register">Register</RouterLink>
+</template>
+
+<style scoped></style>
